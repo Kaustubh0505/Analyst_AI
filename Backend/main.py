d6dc3867742dd029d92020417d1037fa5a259699
Kaustubh Hiwanj <kaustubhhiwanj@Kaustubhs-MacBook-Pro-2.local>
2026-04-09 00:09:09 +0530

    update

--- Backend/main.py
+++ Backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://analystai.kaustubh.codes"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
